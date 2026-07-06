47dfe0f7f6c803a860595fff8c3af65094a3b493
TobeOf17 <ofilitobe@gmail.com>
2026-07-06 02:43:09 +0100

    Initial commit: WhyFi Spring Boot skeleton

---
+++ src/main/java/com/whyfi/WhyfiApplication.java
@@ -0,0 +1,13 @@
+package com.whyfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhyfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhyfiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whyfi/WhyfiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whyfi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhyfiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
