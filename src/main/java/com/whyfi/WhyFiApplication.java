e60502fc65a8dd3f0977a659ac33c6edc86593fa
TobeOf17 <ofilitobe@gmail.com>
2026-07-06 03:04:54 +0100

    Application Renaming

--- src/main/java/com/whyfi/WhyFiApplication.java
+++ src/main/java/com/whyfi/WhyFiApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WhyfiApplication {
+public class WhyFiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WhyfiApplication.class, args);
+		SpringApplication.run(WhyFiApplication.class, args);
 	}
-
 }
